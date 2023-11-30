23fc39b50651e8658b1ab3a4530289539e7e6826
Muhammadumer.eth <umershaikh217@gmail.com>
2023-12-01 01:43:54 +0500

    margin fix

--- src/Components/Sections.jsx
+++ src/Components/Sections.jsx
@@ -817,6 +817,7 @@ const Contact = () => {
             initial={{ opacity: 0, y: 20 }}
             animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
             transition={{ duration: 0.5 }}
+            style={{ marginLeft: "-1rem" }}
           >
             <StyledButton color="#1A1A1A" round="0px">
               Book a call

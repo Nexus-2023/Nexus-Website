d4ef0d04b108f728ee52151abc7628783a6e0136
Muhammadumer.eth <umershaikh217@gmail.com>
2023-12-04 19:41:24 +0500

    responsive footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
         animate={isInView ? { opacity: 1, y: 20 } : { opacity: 0, y: 0 }}
         transition={{ duration: 0.7 }}
       >
-        <div className="h-[30vh] bg-primary text-white flex justify-evenly  p-10 ">
+        <div className="h-[30vh] bg-primary hidden medium:flex text-white  justify-evenly  p-10  ">
           <div className="flex flex-col space-y-2  w-[40vw]">
             <div>
               <Image src={logo} height={170} width={170} />
@@ -35,7 +35,7 @@ const Footer = () => {
             <div className="text-md">© 2023 Nexus Network, Inc.</div>
           </div>
 
-          <div className="flex flex-col text-base space-y-2 ">
+          <div className="flex flex-col text-base space-y-2  mb-16">
             <div className=" text-lg font-semibold">
               <Link href={""}>Links</Link>
             </div>
@@ -54,7 +54,7 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="flex flex-col text-base space-y-2 ">
+          <div className="flex flex-col text-base space-y-2  mb-16">
             <div className=" text-lg font-semibold">
               <Link href={""}>Social</Link>
             </div>
@@ -70,6 +70,58 @@ const Footer = () => {
                   <span> {key}</span>
                 </StyledLink>
               ))}
+            </div>
+          </div>
+        </div>
+
+        {/* mobile*/}
+
+        <div className="h-[50vh] bg-primary flex-col flex medium:hidden medium:h-[30vh] text-white  justify-center items-center p-10 ">
+          <div className="flex flex-col space-y-2 justify-center items-center  mb-[3rem] w-[40vw]">
+            <div>
+              <Image src={logo} height={170} width={170} />
+            </div>
+
+            <div className="text-md">© 2023 Nexus Network, Inc.</div>
+          </div>
+
+          <div className="flex space-x-24 justify-evenly  mb-16">
+            <div className="flex flex-col text-base space-y-2 ">
+              <div className=" text-lg font-semibold">
+                <Link href={""}>Links</Link>
+              </div>
+
+              <div className="text-md flex flex-col space-y-2">
+                {Object.entries(Links).map(([key, value]) => (
+                  <StyledLink
+                    hoverColor="white"
+                    Col="white"
+                    href={value}
+                    target="_blank"
+                  >
+                    {key}
+                  </StyledLink>
+                ))}
+              </div>
+            </div>
+
+            <div className="flex flex-col text-base space-y-2 ">
+              <div className=" text-lg font-semibold">
+                <Link href={""}>Social</Link>
+              </div>
+
+              <div className="text-md flex flex-col space-y-2">
+                {Object.entries(Socials).map(([key, value]) => (
+                  <StyledLink
+                    hoverColor="white"
+                    Col="white"
+                    href={value}
+                    target="_blank"
+                  >
+                    {key}
+                  </StyledLink>
+                ))}
+              </div>
             </div>
           </div>
         </div>

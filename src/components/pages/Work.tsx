202731d6d7f6a2fcb02442f5e4ebfef7d4a2b161
umershaikh123 <umershaikh217@gmail.com>
2024-02-20 23:21:38 +0500

    smooth animation

--- src/components/pages/Work.tsx
+++ src/components/pages/Work.tsx
@@ -12,20 +12,20 @@ export function WorkPage() {
       <div className=" h-full -z-20   bg-[#010305] w-full relative  md:py-24    md:border-x-2 md:border-t-2 border-[#0D1820]   ">
         <div className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 w-[20%] h-[100%] bg-[#38598a2c] rounded-2xl blur-3xl  -z-20 " />
         <div className="  flex items-center justify-center  mt-8 ">
-          <h1 className="sm:text-4xl   font-medium  text-xl  text-wrap text-center  z-10">
+          <h1 className="lg:text-4xl   font-medium  sm:text-2xl text-xl  text-wrap text-center  z-10">
             {" "}
             How does Nexus Network Work?
           </h1>
         </div>
 
-        <div className="flex items-center justify-center -mt-24 -z-10">
+        <div className="flex items-center justify-center md:-mt-16 lg:-mt-24 -mt-8 -z-10">
           <Image
             src={diagram}
             width={0}
             height={0}
             style={{ width: "100%", height: "100%" }}
             alt="Nexus design diagram"
-            className=" aspect-video max-w-screen-xl"
+            className=" aspect-video  "
           />
         </div>
       </div>

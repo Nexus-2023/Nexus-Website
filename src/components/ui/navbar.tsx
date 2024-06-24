7aa83ec76f2466b43f0ce6a5af21bc377b959789
umershaikh123 <umershaikh217@gmail.com>
2024-06-25 04:47:28 +0500

    changes

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -321,9 +321,9 @@ function ResponsiveAppBar() {
 
           <Box>
           <div>
-                <a href="https://www.nexusnetwork.live/" target="_blank">
-              <button className="backdrop-blur mt-2 rounded-3xl  bg-gradient-to-t   from-[var(--scedule-call-from-gradient)]  to-[var(--scedule-call-to-gradient)]  hover:from-[var(--scedule-call-hover-from-gradient)] hover:to-[var(--scedule-call-hover-to-gradient)]    transition  ease-in-out duration-500  hover:scale-110     text-black  font-semibold px-8 py-2 border-2 border-[var(--page-border)]"  title="Nexus orbit Dapp link">
-                app
+                <a href="https://testnet.app.nexusnetwork.live/?destinationChain=nexus-orbit-chain&sourceChain=holesky" target="_blank">
+                <button className="backdrop-blur mt-2 rounded-3xl  bg-gradient-to-t   from-[var(--scedule-call-from-gradient)]  to-[var(--scedule-call-to-gradient)]  hover:from-[var(--scedule-call-hover-from-gradient)] hover:to-[var(--scedule-call-hover-to-gradient)]    transition  ease-in-out duration-500  hover:scale-110     text-black  font-semibold px-6 py-2 border-2 border-[var(--page-border)]" title="Nexus Orbit Dapp">
+                Launch App
               </button>
                 </a>
             </div>

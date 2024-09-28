18c9c118af6d7bbafe94561c97a47ae2b639e74e
umershaikh123 <umershaikh217@gmail.com>
2024-09-28 16:23:12 +0500

    fix : animation trigger

--- src/components/Common/Container.tsx
+++ src/components/Common/Container.tsx
@@ -3,9 +3,9 @@ import ClientFade from "../Client/ClientFade"
 
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClientFade>
+    <ClientFade delay={1000}>
       <div className="justify-center items-center flex  z-10  md:px-16 px-0  mx-auto ">
-        <div className="h-full  w-full flex  py-16  items-center  relative    border-x-2  border-t-2   border-[var(--page-border)]   flex-col  ">
+        <div className="h-full  w-full flex  py-32  items-center  relative    border-x-2  border-t-2   border-[var(--page-border)]   flex-col  ">
           {children}
         </div>
       </div>
